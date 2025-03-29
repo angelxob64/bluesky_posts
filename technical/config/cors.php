9ab3ce9f5ecd048c571198c7592a160cb0750820
angelacolmenares <angelacolmenares@pangostudio.com>
2025-03-29 22:49:16 +0100

    removidos urls ngroks

--- technical/config/cors.php
+++ technical/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','https://6fb8-79-117-245-20.ngrok-free.app'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
